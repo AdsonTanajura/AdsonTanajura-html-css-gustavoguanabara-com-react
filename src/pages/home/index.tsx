8d717c20e468238f0509ea735ac52abaff382b83
AdsonTanajura <adson.tanajura@gmail.com>
2024-08-24 09:51:34 -0300

    Comecando o projeto

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container } from "./styles";
+
+const Home: React.FC = () => {
+
+
+
+    return(
+
+        <Container>
+            <h1>
+            Bem vindo ao meu portfolio de conseito HTML, CSS e JavaScirpt usando o React
+            </h1>
+        </Container>
+
+    )
+
+};
+
+export default Home;
